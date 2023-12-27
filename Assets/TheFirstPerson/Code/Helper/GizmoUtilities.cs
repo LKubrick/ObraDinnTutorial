1cb22d4c85cf1a17ae1680473a1f8fbfd34ce7b1
LKubrick <lorenzo.carly@protonmail.Com>
2023-12-27 04:35:30 +0100

    Switched to 2021.3.4 and target Apple Silicon
    Commented Gizmo Utilities reference in FPSController.cs that made it impossible to build

--- Assets/TheFirstPerson/Code/Helper/GizmoUtilities.cs
+++ Assets/TheFirstPerson/Code/Helper/GizmoUtilities.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
-namespace TheFirstPerson.Helper
+namespace TheFirstPerson.Code.Helper
 {
-    public static class GizmoUtilities
+   /* public static class GizmoUtilities
     {
         //Code by toomasio found at https://forum.unity.com/threads/drawing-capsule-gizmo.354634/
         public static void DrawWireCapsule(Vector3 _pos, Quaternion _rot, float _radius, float _height, Color _color = default(Color))
@@ -33,5 +31,5 @@ namespace TheFirstPerson.Helper
 
             }
         }
-    }
+    }*/
 }
